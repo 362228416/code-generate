21871d20403a3ea3d3a8ecb75cc349abfe3b4361
362228416 <362228416@qq.com>
2013-09-09 16:36:48 +0800

    添加代码生成历史记录，用来修复在单独生成dao,service,controller代码的时候，导包错误的问题，由于时间问题，留到下次在做...

---
+++ code-generate/src/utils/CodeHistoryManager.java
@@ -0,0 +1,36 @@
+package utils;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 
+ * @author john
+ */
+public class CodeHistoryManager {
+
+	static Queue<History> histories = new LinkedList<History>();
+
+	public static void addHistory(String project, String domain, String packageName, String className, CodeType type) {
+		if (histories.size() == 1000) {
+			histories.remove();
+		}
+		History history = new History();
+		history.project = project;
+		history.packageName = packageName;
+		history.domain = domain;
+		history.className = className;
+		history.type = type;
+		histories.add(history);
+	}
+
+}
+
+class History {
+	String project, domain, packageName, className;
+	CodeType type;
+}
+
+enum CodeType {
+	DAO, SERVICE, CONTROLLER
+}
